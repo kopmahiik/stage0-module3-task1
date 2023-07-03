6c100d09aec7f9fae7ebdd5345c2a54a0971ad9b
Aziz Ulmasov <azizulmasov93@gmail.com>
2023-07-03 12:01:39 +0500

    Task is complete

--- src/main/java/school/mjc/stage0/module3/task1/PrintSentenceApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintSentenceApp.java
@@ -2,6 +2,6 @@ package school.mjc.stage0.module3.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
-        System.out.println("This is my first java program");
+        System.out.println("This is my first Java program");
     }
 }
